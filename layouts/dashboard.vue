3c28d02ff35980dc882d1b8769b07f1d40a1676d
Oliver Maglana <olivermaglana@gmail.com>
2024-12-16 23:01:55 +0800

    Add api call on dashboard header

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="flex flex-col min-h-screen">
-		<DashboardHeader />
+		<DashboardHeader :user="user" />
 		<main class="flex-grow w-full mx-auto">
 			<slot />
 		</main>
 		<Footer />
 	</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { user, getCurrentUser } = useAuth();
+getCurrentUser();
+</script>
