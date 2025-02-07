3a806062c5b18ae296fcf927c446edd12a19a233
Oliver Maglana <olivermaglana@gmail.com>
2025-02-07 12:21:11 +0800

    Bug fix logic error for checking session

--- middleware/02.auth.global.ts
+++ middleware/02.auth.global.ts
@@ -8,13 +8,14 @@ export default defineNuxtRouteMiddleware((to, from) => {
 		"/forgot-password",
 		"/reset-password",
 	];
+
 	// When user is not logged in and the route is not excempted, redirect to login
 	if (!isLoggedIn() && !excemptedRoutes.includes(to.path)) {
 		return navigateTo("/login");
 	}
     
 	// When user is logged in and the route is excempted, redirect to home
-	// if (isLoggedIn() && excemptedRoutes.includes(to.path)) {
-	// 	return navigateTo("/");
-	// }
+	if (isLoggedIn() && excemptedRoutes.includes(to.path)) {
+		return navigateTo("/");
+	}
 });
